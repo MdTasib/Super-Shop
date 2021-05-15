ea42bc82212cfb4230839bd4666724d2067b1a7a
MdTasib <ohidulalam442@gmail.com>
2021-05-16 00:35:03 +0600

    firebase setup

---
+++ src/Components/Login/firebase.confiq.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIG6oDTOmjpJlb4T55WrQc1QqJHh6YSow",
+    authDomain: "super-shop-4cec7.firebaseapp.com",
+    projectId: "super-shop-4cec7",
+    storageBucket: "super-shop-4cec7.appspot.com",
+    messagingSenderId: "904657883206",
+    appId: "1:904657883206:web:6877a29b877b02cd13fa47"
+};
+
+export default firebaseConfig;
