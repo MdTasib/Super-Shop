1f4c8ba95d54e0dc88d5a985e3bba2e37c9fabf3
MdTasib <ohidulalam442@gmail.com>
2021-05-16 00:13:33 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from './Components/Home/Home';
+import Orders from './Components/Orders/Orders';
+import Admin from './Components/Admin/Admin';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+          <Route path='/orders'>
+            <Orders />
+          </Route>
+          <Route path='/admin'>
+            <Admin />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to='/home'><b>S</b>UPER <b>S</b>HOP</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ml-auto">
+                            <Link className="nav-link active" aria-current="page" to='/home'>Home</Link>
+                            <Link className="nav-link" to='/orders'>Orders</Link>
+                            <Link className="nav-link" to='/admin'>Admin</Link>
+                            <Link to='/login'><Button variant="contained" color="secondary">Log in</Button></Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <SearchBox />
+        </>
+    );
+};
+
+function SearchBox() {
+    return (
+        <nav class="navbar navbar-light">
+            <form class="form-inline m-auto">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar;
